5c0b9e5f8ac576b67e1966271d4c4d409c96958d
Ogabek Rakhimov <coderogabek@gmail.com>
2023-07-30 21:33:25 +0500

    Several admins feature

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import time
 api_id = config("API_ID")
 api_hash = config("API_HASH")
 bot_token = config("BOT_TOKEN")
-user_id = config("USER_ID")
+user_ids = config("USER_IDS")
 request_sleep_time = config("SLEEP_TIME_EACH_REQUEST")
 loop_sleep_time = config("SLEEP_TIME_EACH_LOOP")
 
